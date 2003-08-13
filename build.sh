345b780e6ecdcfa52d8a482c626b97802a453433
bernard <bernard@75e209cf-7bc6-457b-8506-78d88e7090b0>
2003-08-13 02:26:32 +0000

    added to matrix class
    git-svn-id: svn+ssh://repo/usr/local/svn/trunk/game@2053 75e209cf-7bc6-457b-8506-78d88e7090b0

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-# $Id: build.sh,v 1.1 2003-08-11 23:30:28 bernard Exp $
+# $Id: build.sh,v 1.2 2003-08-13 02:26:32 bernard Exp $
 make clean
 make depend
-../cross-make.sh OPTIMIZE=1
+../cross-make.sh OPTIMIZE=1 -j 2
 cp main.exe main_op.exe
 make clean
-../cross-make.sh PROFILE=1
+../cross-make.sh PROFILE=1 -j 2
 cp main.exe main_pf.exe
 make clean
-../cross-make.sh
+../cross-make.sh -j 2
--- src/matrix.h
+++ src/matrix.h
@@ -1,168 +1,186 @@
 
-// $Id: matrix.h,v 1.1 2003-08-02 18:01:34 bernard Exp $
+// $Id: matrix.h,v 1.2 2003-08-13 02:26:32 bernard Exp $
 
 #ifndef __MATRIX33_H__
 #define __MATRIX33_H__
 
 #include <math.h>
+#include <iostream>
 
 #include "vector.h"
 
-/*
 
-TODO
 
 class matrix33 {
 public:
-   float m[9];
-   float m11, m12, m13, m14;
-   float m21, m22, m23, m24;
-   float m31, m32, m33, m34;
+   float m11, m12, m13;
+   float m21, m22, m23;
+   float m31, m32, m33;
 
-   Matrix() { }
-   Matrix(const Matrix&);
-   Matrix(int, const Vector&, const Vector&, const Vector&, const Vector&);
-   Matrix(int, int, int, const Vector&);
-   ~Matrix() { }
+   matrix33() { m11 = m12 = m13 = 
+                m21 = m22 = m23 = 
+                m31 = m32 = m33 = one; }
+   matrix33(const float r1c1, const float r1c2, const float r1c3, 
+            const float r2c1, const float r2c2, const float r2c3, 
+            const float r3c1, const float r3c2, const float r3c3) {
 
-   void Identity(void);
-   void Scale(const Vector&);
-   void Translate(const Vector&);
-   void RotateX(int);
-   void RotateY(int);
-   void RotateZ(int);
+      m11 = r1c1;   m12 = r1c2;   m13 = r1c3;
+      m21 = r2c1;   m22 = r2c2;   m23 = r2c3;
+      m31 = r3c1;   m32 = r3c2;   m33 = r3c3;
+   }
 
-   void PostScale(const Vector&);
-   void PostTranslate(const Vector&);
-   void PostRotateX(int);
-   void PostRotateY(int);
-   void PostRotateZ(int);
+   ~matrix33() { }
 
-   void Transpose(void);
-   void Inverse(void);
+   float det() const;
+   matrix33 transpose() const;
+   matrix33 inverse() const;
 
-   void RotateAboutVector(const Vector&, int);
-   void AlignToVector(const Vector &);
-
-   friend Vector operator*(const Matrix&, const Vector&);
-   friend Matrix operator*(const Matrix&, const Matrix&);
-
-   Matrix& operator*=(const Matrix&);
+   matrix33 matrix33::operator*=(const float s);
+   matrix33 matrix33::operator/=(const float s);
+   matrix33 matrix33::operator-=(const float s);
+   matrix33 matrix33::operator+=(const float s);
 };
 
+inline float matrix33::det() const {
 
-inline Matrix::Matrix(const Matrix& m) {
-
-   m11 = m.m11;  m12 = m.m12;  m13 = m.m13;  m14 = m.m14;
-   m21 = m.m21;  m22 = m.m22;  m23 = m.m23;  m24 = m.m24;
-   m31 = m.m31;  m32 = m.m32;  m33 = m.m33;  m34 = m.m34;
+   return    m11*m22*m33 -
+             m11*m32*m23 +
+             m21*m32*m13 -
+             m21*m12*m33 +
+             m31*m12*m23 -
+             m31*m22*m13;
 }
 
-inline Matrix::Matrix(int how, const Vector& v1, const Vector& v2, const Vector& v3, const Vector& v4) {
+inline matrix33 matrix33::transpose() const {
 
-   if(how == MAT_ROWS) {
-      m11 = v1.x;
-      m12 = v1.y;
-      m13 = v1.z;
-      m21 = v2.x;
-      m22 = v2.y;
-      m23 = v2.z;
-      m31 = v3.x;
-      m32 = v3.y;
-      m33 = v3.z;
-      m14 = v4.x;
-      m24 = v4.y;
-      m34 = v4.z;
-   }
-   else {
-      m11 = v1.x;
-      m12 = v2.x;
-      m13 = v3.x;
-      m21 = v1.y;
-      m22 = v2.y;
-      m23 = v3.y;
-      m31 = v1.z;
-      m32 = v2.z;
-      m33 = v3.z;
-      m14 = v4.x;
-      m24 = v4.y;
-      m34 = v4.z;
-   }
+   return matrix33( m11, m21, m31,
+                    m12, m22, m32, 
+                    m13, m23, m33  );
 }
 
-inline Matrix::Matrix(int rx, int ry, int rz, const Vector& t) {
 
-   float sx, sy, sz, cx, cy, cz;
 
-   sx = Sin[rx];
-   sy = Sin[ry];
-   sz = Sin[rz];
-   cx = Cos[rx];
-   cy = Cos[ry];
-   cz = Cos[rz];
+inline matrix33 matrix33::inverse() const{
 
-   m11 = cy * cz;
-   m12 = cx * -sz + sx * sy * cz;
-   m13 = -sx * -sz + cx * sy * cz;
-   m14 = t.x;
-   m21 = cy * sz;
-   m22 = cx * cz + sx * sy * sz;
-   m23 = -sx * cz + cx * sy * sz;
-   m24 = t.y;
-   m31 = -sy;
-   m32 = sx * cy;
-   m33 = cx * cy;
-   m34 = t.z;
+   float d = det();
+
+   if(d == zero)
+      d = one;
+
+   return matrix33(  (m22*m33 - m23*m32) / d, 
+                    -(m12*m33 - m13*m32) / d,
+                     (m12*m23 - m13*m22) / d,
+                    -(m21*m33 - m23*m31) / d,
+                     (m11*m33 - m13*m31) / d,
+                    -(m11*m23 - m13*m21) / d,
+                     (m21*m32 - m22*m31) / d,
+                    -(m11*m32 - m12*m31) / d,
+                     (m11*m22 - m12*m21) / d  );
 }
 
-inline Matrix& Matrix::operator*=(const Matrix& b) {
 
-   Matrix a = *this;
+inline matrix33 matrix33::operator*=(const float s) {
 
-   m11 = a.m11 * b.m11 + a.m12 * b.m21 + a.m13 * b.m31; 
-   m12 = a.m11 * b.m12 + a.m12 * b.m22 + a.m13 * b.m32; 
-   m13 = a.m11 * b.m13 + a.m12 * b.m23 + a.m13 * b.m33; 
-   m14 = a.m11 * b.m14 + a.m12 * b.m24 + a.m13 * b.m34 + a.m14;  
-   m21 = a.m21 * b.m11 + a.m22 * b.m21 + a.m23 * b.m31; 
-   m22 = a.m21 * b.m12 + a.m22 * b.m22 + a.m23 * b.m32; 
-   m23 = a.m21 * b.m13 + a.m22 * b.m23 + a.m23 * b.m33; 
-   m24 = a.m21 * b.m14 + a.m22 * b.m24 + a.m23 * b.m34 + a.m24;  
-   m31 = a.m31 * b.m11 + a.m32 * b.m21 + a.m33 * b.m31; 
-   m32 = a.m31 * b.m12 + a.m32 * b.m22 + a.m33 * b.m32; 
-   m33 = a.m31 * b.m13 + a.m32 * b.m23 + a.m33 * b.m33; 
-   m34 = a.m31 * b.m14 + a.m32 * b.m24 + a.m33 * b.m34 + a.m34;
+   m11 *= s;   m12 *= s;   m13 *= s;
+   m21 *= s;   m22 *= s;   m23 *= s;
+   m31 *= s;   m32 *= s;   m33 *= s;
 
-   return(*this);
+   return *this;
 }
 
-inline Matrix operator*(const Matrix& a, const Matrix& b) {
+inline matrix33 matrix33::operator/=(const float s) {
 
-   Matrix c;
+   m11 /= s;   m12 /= s;   m13 /= s;
+   m21 /= s;   m22 /= s;   m23 /= s;
+   m31 /= s;   m32 /= s;   m33 /= s;
 
-   c.m11 = a.m11 * b.m11 + a.m12 * b.m21 + a.m13 * b.m31; 
-   c.m12 = a.m11 * b.m12 + a.m12 * b.m22 + a.m13 * b.m32; 
-   c.m13 = a.m11 * b.m13 + a.m12 * b.m23 + a.m13 * b.m33; 
-   c.m14 = a.m11 * b.m14 + a.m12 * b.m24 + a.m13 * b.m34 + a.m14;  
-   c.m21 = a.m21 * b.m11 + a.m22 * b.m21 + a.m23 * b.m31; 
-   c.m22 = a.m21 * b.m12 + a.m22 * b.m22 + a.m23 * b.m32; 
-   c.m23 = a.m21 * b.m13 + a.m22 * b.m23 + a.m23 * b.m33; 
-   c.m24 = a.m21 * b.m14 + a.m22 * b.m24 + a.m23 * b.m34 + a.m24;  
-   c.m31 = a.m31 * b.m11 + a.m32 * b.m21 + a.m33 * b.m31; 
-   c.m32 = a.m31 * b.m12 + a.m32 * b.m22 + a.m33 * b.m32; 
-   c.m33 = a.m31 * b.m13 + a.m32 * b.m23 + a.m33 * b.m33; 
-   c.m34 = a.m31 * b.m14 + a.m32 * b.m24 + a.m33 * b.m34 + a.m34;
-
-   return(c);
+   return *this;
 }
 
-inline Vector operator*(const Matrix& m, const Vector& v) {
+inline matrix33 matrix33::operator+=(const float s) {
 
-   return(Vector(m.m11*v.x + m.m12*v.y + m.m13*v.z + m.m14,
-                 m.m21*v.x + m.m22*v.y + m.m23*v.z + m.m24,   
-                 m.m31*v.x + m.m32*v.y + m.m33*v.z + m.m34));
+   m11 += s;   m12 += s;   m13 += s;
+   m21 += s;   m22 += s;   m23 += s;
+   m31 += s;   m32 += s;   m33 += s;
+
+   return *this;
 }
 
-*/
 
+inline matrix33 matrix33::operator-=(const float s) {
+
+   m11 -= s;   m12 -= s;   m13 -= s;
+   m21 -= s;   m22 -= s;   m23 -= s;
+   m31 -= s;   m32 -= s;   m33 -= s;
+
+   return *this;
+}
+
+
+
+inline matrix33 operator*(const matrix33& a, const matrix33& b) {
+
+   return matrix33( a.m11 * b.m11 + a.m12 * b.m21 + a.m13 * b.m31, 
+                    a.m11 * b.m12 + a.m12 * b.m22 + a.m13 * b.m32,
+                    a.m11 * b.m13 + a.m12 * b.m23 + a.m13 * b.m33,
+                    a.m21 * b.m11 + a.m22 * b.m21 + a.m23 * b.m31,
+                    a.m21 * b.m12 + a.m22 * b.m22 + a.m23 * b.m32,
+                    a.m21 * b.m13 + a.m22 * b.m23 + a.m23 * b.m33,
+                    a.m31 * b.m11 + a.m32 * b.m21 + a.m33 * b.m31,
+                    a.m31 * b.m12 + a.m32 * b.m22 + a.m33 * b.m32,
+                    a.m31 * b.m13 + a.m32 * b.m23 + a.m33 * b.m33  );
+
+}
+
+inline matrix33 operator/(const matrix33& m, float s) {
+
+   return matrix33( m.m11/s, m.m12/s, m.m13/s, 
+                    m.m21/s, m.m22/s, m.m23/s, 
+                    m.m31/s, m.m32/s, m.m33/s  );
+}
+
+
+inline matrix33 operator*(const matrix33& m, float s) {
+
+   return matrix33( m.m11*s, m.m12*s, m.m13*s, 
+                    m.m21*s, m.m22*s, m.m23*s, 
+                    m.m31*s, m.m32*s, m.m33*s  );
+}
+
+inline matrix33 operator*(float s, const matrix33& m) {
+
+   return m * s;
+}
+
+
+
+inline vector3 operator*(const matrix33& m, const vector3& v) {
+
+   return vector3( m.m11*v.x + m.m12*v.y + m.m13*v.z,
+                   m.m21*v.x + m.m22*v.y + m.m23*v.z,   
+                   m.m31*v.x + m.m32*v.y + m.m33*v.z  );
+}
+
+inline vector3 operator*(const vector3& v, const matrix33& m) {
+
+   return vector3( v.x*m.m11 + v.y*m.m21 + v.z*m.m31, 
+                   v.x*m.m12 + v.y*m.m22 + v.z*m.m32, 
+                   v.x*m.m13 + v.y*m.m23 + v.z*m.m33  );
+}
+
+
+
+inline std::ostream& operator<<(std::ostream& s, const matrix33& m) {
+
+   s << "({" << m.m11 << "," << m.m12 << "," << m.m13 << "},{"
+             << m.m21 << "," << m.m22 << "," << m.m23 << "},{"
+             << m.m31 << "," << m.m32 << "," << m.m33 << "})";
+
+   return s;
+}
+
+
+
+  
 #endif
 
